fb17e458d898b933830a80c2b13e4c770175cfa1
Simon Rey <reysimon@orange.fr>
2024-03-07 11:20:43 +0100

    Adding metadata as CSV

--- scripts/metadata/compute_metadata.py
+++ scripts/metadata/compute_metadata.py
@@ -11,45 +11,37 @@ from preflibtools.properties import num_alternatives, num_voters, num_different_
     is_single_peaked_ILP
 
 
-all_orders = ("soc", "soi", "toc", "toi")
-all_orders_and_cat = ("soc", "soi", "toc", "toi", "cat")
-METADATAS = {
-    "file": {"appliesTo": None, "function": lambda x: x.file_name},
-    "numAlt": {"appliesTo": None, "function": num_alternatives},
-    "numVot": {"appliesTo": None, "function": num_voters},
-    "numUniq": {"appliesTo": all_orders_and_cat, "function": num_different_preferences},
-    "isStrict": {"appliesTo": all_orders, "function": is_strict},
-    "isComplete": {"appliesTo": all_orders_and_cat, "function": is_complete},
-    "isApproval": {"appliesTo": all_orders_and_cat, "function": is_approval},
-    "largestBallot": {"appliesTo": all_orders_and_cat, "function": largest_ballot},
-    "smallestBallot": {"appliesTo": all_orders_and_cat, "function": smallest_ballot},
-    "maxNumIndif": {"appliesTo": all_orders_and_cat, "function": max_num_indif},
-    "minNumIndif": {"appliesTo": all_orders_and_cat, "function": min_num_indif},
-    "largestIndif": {"appliesTo": all_orders_and_cat, "function": largest_indif},
-    "smallestIndif": {"appliesTo": all_orders_and_cat, "function": smallest_indif},
-    "hasCondorcet": {"appliesTo": all_orders, "function": has_condorcet},
-    "isSP": {"appliesTo": ("soc_aaaa", "toc_aaaa"), "function": {
-        "soc": lambda x: is_single_peaked(x)[0],
-        "toc": lambda x: is_single_peaked_ILP(x)[0]}},
-    "isSC": {"appliesTo": ("soc_aaaa",), "function": is_single_crossing},
-}
-HEADERS = list(METADATAS)
+class Property:
+    def __init__(self, name, applies_to, func):
+        self.name = name
+        self.applies_to = applies_to
+        self.func = func
+
+    def applies_to_instance(self, instance):
+        return self.applies_to is None or instance.data_type in self.applies_to
+
+    def applies_to_file(self, file):
+        return self.applies_to is None or os.path.splitext(file)[1][1:] in self.applies_to
+
+    def compute_instance(self, instance):
+        if self.applies_to_instance(instance):
+            if isinstance(self.func, dict):
+                return self.func[instance.data_type](instance)
+            else:
+                return self.func(instance)
 
 
-def compute_file_metadata(file_path):
+def compute_file_properties(file_path, all_properties):
     if file_path[-3:] not in ("soc", "soi", "toc", "toi", "cat", "wmd"):
         return None
     file_name = os.path.basename(file_path)
     print(f"\t{file_name}")
     instance = get_parsed_instance(file_path)
     results = {}
-    for meta, meta_info in METADATAS.items():
-        if meta_info["appliesTo"] is None or instance.data_type in meta_info["appliesTo"]:
-            func = meta_info["function"]
-            if isinstance(func, dict):
-                results[meta] = func[instance.data_type](instance)
-            else:
-                results[meta] = func(instance)
+    for prop in all_properties:
+        res = prop.compute_instance(instance)
+        if res is not None:
+            results[prop.name] = res
     return results
 
 
@@ -61,7 +53,7 @@ def compute_dataset_metadata(root_dir_path):
         out_file.write(";".join(HEADERS) + "\n")
 
     # for file in info["files"]:
-    #     metadata = compute_file_metadata(os.path.join(root_dir_path, file))
+    #     metadata = compute_file_properties(os.path.join(root_dir_path, file))
     #     if metadata is not None:
     #         with open(out_file_path, "a") as out_file:
     #             meta_write = [str(metadata[h]) if h in metadata else "N/A" for h in HEADERS]
@@ -72,7 +64,7 @@ def compute_dataset_metadata(root_dir_path):
     for file in info["files"]:
         files.append(os.path.join(root_dir_path, file))
 
-    for metadata in file_pool.imap_unordered(compute_file_metadata, files):
+    for metadata in file_pool.imap_unordered(compute_file_properties, files):
         if metadata is not None:
             with open(out_file_path, "a") as out_file:
                 meta_write = [str(metadata[h]) if h in metadata else "N/A" for h in HEADERS]
@@ -100,10 +92,52 @@ def write_all_metadata_files(all_metadata, root_dir_path):
     for ds_dir, metadata in all_metadata.items():
         metadata.to_csv(
             os.path.join(root_dir_path, ds_dir, "metadata.csv"),
-            delimiter=";",
-            encoding="utf-8"
+            sep=";",
+            na_rep="N/A",
+            index=False,
+            encoding="utf-8",
         )
+        print(f"Wrote the metadata file for {ds_dir}.")
 
+
+def add_property_value(all_metadata, root_dir_path, property_obj):
+    def apply_on_instance(df_row):
+        file = df_row["file"]
+        if property_obj.applies_to_file(file):
+            instance = get_parsed_instance(os.path.join(root_dir_path, ds_dir, file))
+            return property_obj.compute_instance(instance)
+
+    print(f"Computing {property_obj.name} for datasets:")
+    for ds_dir, metadata in all_metadata.items():
+        print(f"\t{ds_dir} with {len(metadata)} files")
+        if len(metadata) > 0:
+            metadata[property_obj.name] = metadata.apply(apply_on_instance, axis=1)
+        break
+
+
+all_orders = ("soc", "soi", "toc", "toi")
+all_orders_and_cat = ("soc", "soi", "toc", "toi", "cat")
+ALL_PROPERTIES = [
+    Property("file", None, lambda x: x.file_name),
+    Property("numAlt", None, num_alternatives),
+    Property("numVot", None, num_voters),
+    Property("numUniq", all_orders_and_cat, num_different_preferences),
+    Property("isStrict", all_orders, is_strict),
+    Property("isComplete", all_orders_and_cat, is_complete),
+    Property("isApproval", all_orders_and_cat, is_approval),
+    Property("largestBallot", all_orders_and_cat, largest_ballot),
+    Property("smallestBallot", all_orders_and_cat, smallest_ballot),
+    Property("maxNumIndif", all_orders_and_cat, max_num_indif),
+    Property("minNumIndif", all_orders_and_cat, min_num_indif),
+    Property("largestIndif", all_orders_and_cat, largest_indif),
+    Property("smallestIndif", all_orders_and_cat, smallest_indif),
+    Property("hasCondorcet", all_orders, has_condorcet),
+    Property("isSP", ("soc_aaaa", "toc_aaaa"), {
+        "soc": lambda x: is_single_peaked(x)[0],
+        "toc": lambda x: is_single_peaked_ILP(x)[0]}),
+    Property("isSC", ("soc_aaaa",), is_single_crossing),
+]
+HEADERS = [m.name for m in ALL_PROPERTIES]
 
 DATASET_FOLDER = os.path.join("..", "..", "datasets")
 
@@ -112,6 +146,8 @@ if __name__ == '__main__':
 
     all_meta = read_all_metadata_files(DATASET_FOLDER)
     # print(all_meta)
+    add_property_value(all_meta, DATASET_FOLDER, ALL_PROPERTIES[0])
+    write_all_metadata_files(all_meta, DATASET_FOLDER)
 
     # for ds in datasets:
     #     compute_dataset_metadata(ds)
